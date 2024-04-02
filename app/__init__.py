0b7e929cfcad995cd905ba2796c52f95d080ab08
zbwer <1641064392@qq.com>
2024-04-02 21:03:35 +0800

    ✨ feat: 新增商品识别接口（待完善）

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+# from flask_sqlalchemy import SQLAlchemy
+from app.Product import ProductPrediction
 from config import Config
 from .routes import HelloWorld, FaceAuth, FaceVerify
 from .Face import FaceManager
@@ -15,11 +17,15 @@ def create_app():
     FaceManager(app.config['KNOWN_FACES_DIR'])
     # 跨域
     CORS(app)
+    # 数据库连接
+    # db = SQLAlchemy(app)
+    # app._g['db'] = db
+
     # JWT 与 路由
     JWTManager(app)
     api = Api(app)
     api.add_resource(HelloWorld, '/hello')
     api.add_resource(FaceAuth, '/face-auth')
     api.add_resource(FaceVerify, '/auth')
-
+    api.add_resource(ProductPrediction, '/predict')
     return app
