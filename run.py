31d001ea84633c6a49c26a8a02db40c7d0649025
zbwer <1641064392@qq.com>
2024-04-21 01:20:52 +0800

    🐞 fix: 外网无法访问接口

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(port=app.config['PORT'])
+    app.run(
+        port=app.config['PORT'],
+        host=app.config['HOST'],
+    )
